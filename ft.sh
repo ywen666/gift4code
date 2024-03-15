fa6d63f794283bbb3b17025f954baa094774be69
Yeming Wen <ywen@c301-001.ls6.tacc.utexas.edu>
2024-03-14 22:25:06 -0500

    change hparams

--- ft.sh
+++ ft.sh
@@ -1,20 +1,21 @@
 DATA_PATH="gift4code.tfrecord"
 OUTPUT_PATH="results"
 MODEL_PATH="deepseek-ai/deepseek-coder-6.7b-instruct"
+MODEL_PATH="deepseek-ai/deepseek-coder-33b-instruct"
 
 #CUDA_VISIBLE_DEVICES=0 python finetune.py \
 deepspeed finetune.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $DATA_PATH \
     --output_dir $OUTPUT_PATH \
-    --num_train_epochs 3 \
+    --num_train_epochs 5 \
     --model_max_length 1024 \
     --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 100 \
+    --save_steps 200 \
     --save_total_limit 100 \
     --learning_rate 2e-5 \
     --warmup_steps 10 \
